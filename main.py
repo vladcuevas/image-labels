975df0622ed420d0e8de5c880af51625fae986d5
vladcuevas <vladcuevas@gmail.com>
2022-06-25 14:25:24 -0500

    adding a few filters

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import io
+import re
 import os
 
 def detect_labels(path):
@@ -19,13 +19,20 @@ def detect_labels(path):
     # Performs label detection on the image file
     response = client.label_detection(image=image)
     labels = response.label_annotations
-    print('Labels:')
 
     keywords = []
     for label in labels:
         keywords.append(label.description)
 
-    print(keywords)
+    with open(f"{path}.txt", 'w', encoding='UTF-8', newline='\n') as f:
+        #keywords
+        f.write(", ".join(keywords))
+        f.write("\n")
+        #hashtags
+        out0 = [x.replace(' ', '') for x in keywords]
+        out0 = f'#{" ".join(out0)}'
+        out0 = re.sub(r'-', '', out0)
+        f.write(re.sub(r'\s', ' #', out0))
 
     if response.error.message:
         raise Exception(
@@ -34,7 +41,22 @@ def detect_labels(path):
                 response.error.message))
 
 
-# The name of the image file to annotate
-file_name = os.path.abspath('resources/wakeupcat.jpg')
+# The name of the images file to annotate
 
-detect_labels(file_name)
+directory = "./resources/"
+
+for root, subdirectories, files in os.walk(directory):
+
+    # extensions
+    ext = ('.JPG', 'jpg')
+
+    for file in files:
+
+        print(os.path.join(root, file))
+        file_name = os.path.abspath(os.path.join(root, file))
+
+        if file_name.endswith(ext):
+
+            detect_labels(file_name)
+
+            print('done')
